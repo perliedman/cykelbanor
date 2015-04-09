1c65071e34dfc4fa0374bb5b73ebb8cc5af755c2
Per Liedman <per@liedman.net>
2015-04-09 23:08:19 +0200

    Preparation script to process elevation

--- scripts/prepare-data.sh
+++ scripts/prepare-data.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 OSRM_DIR=/home/per/Documents/Projects/osrm-backend/build
+OSM_SLOPE="node /home/per/Documents/Projects/osm-slope/index.js"
 DATA_DIR=/home/per/Documents/geodata
 DATA=$DATA_DIR/sweden
 DATA_OSM=$DATA.osm.pbf
 DATA_OSRM=$DATA.osrm
+DATA_SLOPE=$DATA"_slope.json"
 PROFILE=./config/bicycle.lua
 
+$OSM_SLOPE --cache_dir $DATA_DIR $DATA_OSM $DATA_SLOPE
 $OSRM_DIR/osrm-extract -p $PROFILE $DATA_OSM && $OSRM_DIR/osrm-prepare -p $PROFILE $DATA_OSRM
