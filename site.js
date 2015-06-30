fed6e504ddf671f04d4f9ac81604b02639ac05e2
Per Liedman <per@liedman.net>
2015-06-30 23:10:08 +0200

    Update site bundle

--- site.js
+++ site.js
@@ -26525,12 +26525,19 @@ module.exports = L.Class.extend({
                 .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
         var data = geojson.coordinates.reduce(function(a, c) {
-            var ll = L.latLng(c[1], c[0]),
+            var ll = L.latLng([c[1], c[0], c[2]]),
+                elevDiff,
                 dist;
 
             if (a.lastLatLng) {
                 dist = a.lastLatLng.distanceTo(ll);
                 a.accDist += dist;
+                elevDiff = ll.alt - a.lastLatLng.alt;
+                if (elevDiff > 0) {
+                    a.accClimb += elevDiff;
+                } else {
+                    a.accDesc -= elevDiff;
+                }
             }
 
             a.data.push({
@@ -26541,17 +26548,29 @@ module.exports = L.Class.extend({
             return a;
         }, {
             accDist: 0,
+            accClimb: 0,
+            accDesc: 0,
             data: []
-        }).data;
+        });
+
+        var statsElem = L.DomUtil.create('div', 'stats', this._container),
+            gramCO2 = data.accDist * 190.280 / 1000;
+        statsElem.innerHTML =
+            L.Util.template('&#9650;{climb} m, &#9660;{desc} m, sparar {co2} {co2unit} CO<sub>2</sub>', {
+                climb: Math.round(data.accClimb),
+                desc: Math.round(data.accDesc),
+                co2: gramCO2 < 1000 ? Math.round(gramCO2) : Math.round(gramCO2 / 100) / 10,
+                co2unit: gramCO2 < 1000 ? 'g' : 'kg'
+            });
 
         // Scale the range of the data
-        x.domain([0, d3.max(data, function(d) { return d.distance; })]);
-        y.domain([0, Math.ceil(d3.max(data, function(d) { return d.elevation; }) / 50) * 50]);
+        x.domain([0, d3.max(data.data, function(d) { return d.distance; })]);
+        y.domain([0, Math.ceil(d3.max(data.data, function(d) { return d.elevation; }) / 50) * 50]);
      
         // Add the valueline path.
         svg.append('path')
             .attr('class', 'line')
-            .attr('d', valueline(data));
+            .attr('d', valueline(data.data));
      
         // Add the X Axis
         svg.append('g')
@@ -26563,6 +26582,8 @@ module.exports = L.Class.extend({
         svg.append('g')
             .attr('class', 'y axis')
             .call(yAxis);
+
+
     },
 
     clear: function() {
