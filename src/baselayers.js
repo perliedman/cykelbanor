3570ad2de32bbac71d4a7f8d3e0b2f3e342f9f11
Per Liedman <per.liedman@dotnetmentor.se>
2016-02-26 16:48:48 +0100

    First steps towards "show nearby"

--- src/baselayers.js
+++ src/baselayers.js
@@ -1,7 +1,8 @@
-var L = require('leaflet');
+var L = require('leaflet'),
+    config = require('./config');
 
 module.exports = {
-    'Karta': L.tileLayer('https://a.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibGllZG1hbiIsImEiOiI1TXRSbUI4In0.EMQ3W8jAteath85pR800ag', {
+    'Karta': L.tileLayer('https://a.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=' + config.mapboxToken, {
         attribution: 'Kartdata &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }),
     'Flygfoto': L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
---
+++ src/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mapboxToken: 'pk.eyJ1IjoibGllZG1hbiIsImEiOiI1TXRSbUI4In0.EMQ3W8jAteath85pR800ag'
+};
