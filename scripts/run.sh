9d4e4ad74238ea14e5023fc9f14707555f8b5a35
Per Liedman <per@liedman.net>
2016-03-06 09:27:07 +0100

    Breakout moment and deps correctly

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./scripts/build.sh
-watchify -t hbsfy -x opening_hours -x moment src/index.js -o site.js &
+watchify -t hbsfy -x opening_hours -x moment -x moment/locale/sv src/index.js -o site.js &
 http-server
