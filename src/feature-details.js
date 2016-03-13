9a2bd5a4408ed6774c69a07243add15f65b89467
Per Liedman <per@liedman.net>
2016-03-13 17:47:24 +0100

    More OSM feature details and I18N

--- src/feature-details.js
+++ src/feature-details.js
@@ -1,7 +1,45 @@
 var template = require('../templates/osm-feature-details.hbs'),
     Handlebars = require('hbsfy/runtime'),
+    restaurantCuisineI18N = {
+        'indian': 'Indisk restaurang',
+        'pizza': 'Pizza-restaurang',
+        'burger': 'Hamburgerrestaurang',
+        'greek': 'Grekisk restaurang',
+        'turkish': 'Turkisk restaurang',
+        'chinese': 'Kinarestaurang',
+        'vietnamese': 'Vietnamesisk restaurang',
+        'thai': 'Thailändsk restaurang',
+        'asian': 'Asiatisk restaurang',
+        'sushi': 'Sushi-restaurang'
+    },
+    fastfoodCuisineI18N = {
+        'pizza': 'Pizzeria',
+        'burger': 'Hamburgare',
+        'thai': 'Thai-mat'
+    },
     amenityI18N = {
-        'restaurant': 'Restaurang'
+        'restaurant': function(f) {
+            var cuisine = restaurantCuisineI18N[f.cuisine];
+            return cuisine ? cuisine : 'Restaurang' + (f.cuisine ? ' (' + f.cuisine + ')' : '');
+        },
+        'fast_food': function(f) {
+            var cuisine = fastfoodCuisineI18N[f.cuisine];
+            return cuisine ? cuisine : 'Snabbmat' + (f.cuisine ? ' (' + f.cuisine + ')' : '');
+        },
+        'cafe': function() { return 'Kafé'; },
+        'veterinary': function() { return 'Veterinär'; },
+        'fuel': function() { return 'Bensinmack'; },
+        'bicycle_rental': function() { return 'Hyrcyklar'; }
+    },
+    shopI18N = {
+        'hardware': function() { return 'Järnaffär'; },
+        'bathroom_furnishing': function() { return 'Badrumsinredning'; },
+        'outdoor': function() { return 'Vildmarksaffär'; },
+        'car_repair': function() { return 'Bilverkstad'; },
+        'clothes': function() { return 'Klädaffär'; },
+        'hairdresser': function() { return 'Frisör'; },
+        'shoes': function() { return 'Skoaffär'; },
+        'confectionery': function() { return 'Godisaffär'; }
     },
     initialUpperCase = function(s) {
         return s.substring(0, 1).toUpperCase() + s.substring(1);
@@ -23,10 +61,14 @@ Handlebars.registerHelper('osmFeatureDescription', function(f) {
     var result = '';
 
     if (f.amenity) {
-        result = amenityI18N[f.amenity] || f.amenity.replace('_', ' ');
+        var amenityFn = amenityI18N[f.amenity];
+        result = (amenityFn ? amenityFn(f) : null) || f.amenity;
+    } else if (f.shop) {
+        var shopFn = shopI18N[f.shop];
+        result = (shopFn ? shopFn(f) : null) || f.shop;
     }
 
-    return initialUpperCase(result);
+    return initialUpperCase(result.replace('_', ' '));
 });
 
 Handlebars.registerHelper('osmOpeningHours', function(f) {
