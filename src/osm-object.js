9f8404d9ee91799b8ad8f0a33fca82e042b2d684
Per Liedman <per.liedman@dotnetmentor.se>
2016-04-22 13:24:52 +0200

    http -> https

--- src/osm-object.js
+++ src/osm-object.js
@@ -2,7 +2,7 @@ var reqwest = require('reqwest');
 
 module.exports = function(type, osmId, cb) {
     reqwest({
-        url: 'http://www.openstreetmap.org/api/0.6/' + type + '/' + osmId,
+        url: 'https://www.openstreetmap.org/api/0.6/' + type + '/' + osmId,
         crossOrigin: true,
         type: 'xml'
     }).then(function(data) {
--- src/poi-layer.js
+++ src/poi-layer.js
@@ -1,7 +1,7 @@
 var underneath = require('leaflet-underneath'),
     config = require('./config');
 
-module.exports = new underneath('http://{s}.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/' +
+module.exports = new underneath('https://{s}.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6/' +
     '{z}/{x}/{y}.vector.pbf?access_token=' + config.mapboxToken, {
     layers: ['poi_label'],
     lazy: true,
