2b1911e0bf45fab0cae1b4180fc7cf7f3b07d000
Per Liedman <per.liedman@dotnetmentor.se>
2018-02-09 15:56:27 +0100

    Actually add elevationToken to config :(

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mapboxToken: 'pk.eyJ1IjoibGllZG1hbiIsImEiOiI1TXRSbUI4In0.EMQ3W8jAteath85pR800ag'
+    mapboxToken: 'pk.eyJ1IjoibGllZG1hbiIsImEiOiI1TXRSbUI4In0.EMQ3W8jAteath85pR800ag',
+    elevationToken: '0555109ea6bd0e7611e62d755475f9e7'
 };
