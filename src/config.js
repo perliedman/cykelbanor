2069ee499b0b0a003514dcc9984e794416b9961d
Per Liedman <per@liedman.net>
2019-10-21 14:33:51 +0200

    Cycle token

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mapboxToken: 'pk.eyJ1IjoibGllZG1hbiIsImEiOiI1TXRSbUI4In0.EMQ3W8jAteath85pR800ag',
+    mapboxToken: 'pk.eyJ1IjoibGllZG1hbiIsImEiOiJjazIwZWk0dWMweWVkM2NtdjBrdXlodWcwIn0.otHbOG0WyCoOoXeZYCe6AA',
     elevationToken: '0555109ea6bd0e7611e62d755475f9e7'
 };
